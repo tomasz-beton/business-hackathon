5da06fdae0d046eacac59f6d46d8dbedd4225ebd
Wojciech Jasiński <woj.jasinski@gmail.com>
2023-05-21 14:06:06 +0200

    black formatting

--- file_utils.py
+++ file_utils.py
@@ -2,40 +2,55 @@ import pandas as pd
 
 
 def load_dataframes(dataset_dir: str):
-    items = pd.read_csv(f'{dataset_dir}/items.csv', na_values='unknown',
-                        dtype={'item_id': 'Int64', 'alley_id': 'Int64', 'category': 'Int64'},
-                        index_col=['item_id', 'alley_id', 'category'])
-    items.drop(columns='Unnamed: 0', inplace=True)
+    items = pd.read_csv(
+        f"{dataset_dir}/items.csv",
+        na_values="unknown",
+        dtype={"item_id": "Int64", "alley_id": "Int64", "category": "Int64"},
+        index_col="item_id",
+    )
+    items.drop(columns="Unnamed: 0", inplace=True)
 
-    categories = pd.read_csv('data/categories.csv', index_col='category_id')
-    categories.drop(columns='Unnamed: 0', inplace=True)
+    categories = pd.read_csv("data/categories.csv", index_col="category_id")
+    categories.drop(columns="Unnamed: 0", inplace=True)
 
-    alley_inventory = pd.read_csv('data/alley_inventory.csv', index_col='alley_id')
+    alley_inventory = pd.read_csv("data/alley_inventory.csv", index_col="alley_id")
 
-    transaction_items__prior = pd.read_csv('data/transaction_items__prior.csv', na_values='unknown',
-                                           dtype={'transaction_id': 'Int64', 'item_id': 'Int64',
-                                                  'add_to_cart_order': 'Int64', 'previous_bought': 'Int64'})
+    transaction_items__prior = pd.read_csv(
+        "data/transaction_items__prior.csv",
+        na_values="unknown",
+        dtype={"transaction_id": "Int64", "item_id": "Int64", "add_to_cart_order": "Int64", "previous_bought": "Int64"},
+    )
 
-    transaction_items__prior.drop(columns='Unnamed: 0', inplace=True)
-    transaction_items__prior.dropna(subset=['transaction_id'], inplace=True)
-    transaction_items__prior.set_index(['transaction_id', 'item_id'], inplace=True)
+    transaction_items__prior.drop(columns="Unnamed: 0", inplace=True)
+    transaction_items__prior.dropna(subset=["transaction_id"], inplace=True)
+    transaction_items__prior.set_index("transaction_id", inplace=True)
 
-    transaction_items__train = pd.read_csv('data/transaction_items__train.csv', na_values='unknown',
-                                           dtype={'transaction_id': 'Int64', 'item_id': 'Int64',
-                                                  'add_to_cart_order': 'Int64', 'previous_bought': 'Int64'})
+    transaction_items__train = pd.read_csv(
+        "data/transaction_items__train.csv",
+        na_values="unknown",
+        dtype={"transaction_id": "Int64", "item_id": "Int64", "add_to_cart_order": "Int64", "previous_bought": "Int64"},
+    )
 
-    transaction_items__train.drop(columns='Unnamed: 0', inplace=True)
-    transaction_items__train.dropna(subset=['transaction_id'], inplace=True)
-    transaction_items__train.set_index(['transaction_id', 'item_id'], inplace=True)
+    transaction_items__train.drop(columns="Unnamed: 0", inplace=True)
+    transaction_items__train.dropna(subset=["transaction_id"], inplace=True)
+    transaction_items__train.set_index("transaction_id", inplace=True)
 
-    transactions = pd.read_csv('data/transactions.csv', na_values='unknown',
-                               dtype={'transaction_id': 'Int64', 'customer_id': 'Int64', 'transaction_number': 'Int64',
-                                      'day_of_week': 'Int64', 'time_of_day': 'Int64',
-                                      'days_since_prior_order': 'Int64'})
+    transactions = pd.read_csv(
+        "data/transactions.csv",
+        na_values="unknown",
+        dtype={
+            "transaction_id": "Int64",
+            "customer_id": "Int64",
+            "transaction_number": "Int64",
+            "day_of_week": "Int64",
+            "time_of_day": "Int64",
+            "days_since_prior_order": "Int64",
+        },
+    )
 
-    transactions.drop(columns='Unnamed: 0', inplace=True)
-    transactions.dropna(subset=['transaction_id'], inplace=True)
-    transactions.set_index(['transaction_id', 'customer_id'], inplace=True)
+    transactions.drop(columns="Unnamed: 0", inplace=True)
+    transactions.dropna(subset=["transaction_id"], inplace=True)
+    transactions.set_index("transaction_id", inplace=True)
 
     dataframes = [items, categories, alley_inventory, transaction_items__prior, transaction_items__train, transactions]
 
--- predict.py
+++ predict.py
@@ -2,29 +2,38 @@ import pickle
 import argparse
 import pandas as pd
 
-def load_transactions(path):
-    transactions = pd.read_csv(path, na_values='unknown',
-                               dtype={'transaction_id': 'Int64', 'customer_id': 'Int64', 'transaction_number': 'Int64',
-                                      'day_of_week': 'Int64', 'time_of_day': 'Int64',
-                                      'days_since_prior_order': 'Int64'})
 
-    transactions.drop(columns='Unnamed: 0', inplace=True)
-    transactions.dropna(subset=['transaction_id'], inplace=True)
-    transactions.set_index('transaction_id', inplace=True)
+def load_transactions(path):
+    transactions = pd.read_csv(
+        path,
+        na_values="unknown",
+        dtype={
+            "transaction_id": "Int64",
+            "customer_id": "Int64",
+            "transaction_number": "Int64",
+            "day_of_week": "Int64",
+            "time_of_day": "Int64",
+            "days_since_prior_order": "Int64",
+        },
+    )
+
+    transactions.drop(columns="Unnamed: 0", inplace=True)
+    transactions.dropna(subset=["transaction_id"], inplace=True)
+    transactions.set_index("transaction_id", inplace=True)
 
     return transactions
 
 
 def prepare_data(transactions):
-    df = client_item_features.reset_index(1).join(transactions.reset_index().set_index('customer_id'), how='inner')
-    df = df.join(client_features, how='inner', rsuffix='__client')
-    df = df.join(item_features, how='inner', on='item_id', rsuffix='__item')
-    df = df.set_index('item_id', append=True)
+    df = client_item_features.reset_index(1).join(transactions.reset_index().set_index("customer_id"), how="inner")
+    df = df.join(client_features, how="inner", rsuffix="__client")
+    df = df.join(item_features, how="inner", on="item_id", rsuffix="__item")
+    df = df.set_index("item_id", append=True)
     df = df.drop(columns=["transaction_number", "eval_set", "days_since_prior_order", "transaction_id"])
     df = df.rename(
         columns={
-            'n_transactions': 'n_transactions__client_item',
-            'n_transactions__client': 'n_transactions',
+            "n_transactions": "n_transactions__client_item",
+            "n_transactions__client": "n_transactions",
             "reorder_rate": "reorder_rate__client_item",
             "reorder_rate__item": "reorder_rate",
         }
@@ -32,32 +41,32 @@ def prepare_data(transactions):
 
     return df
 
+
 def predict_and_order(df, model, threshold=0.5, limit=5):
     df["prediction"] = model.predict_proba(df)[:, 1]
 
-    df = df.sort_values(by=['customer_id', 'prediction'], ascending=[True, False])
+    df = df.sort_values(by=["customer_id", "prediction"], ascending=[True, False])
     df = df.loc[df.prediction > threshold]
-    preds_df = df.reset_index(1).groupby('customer_id').agg(
-        {
-            'item_id': lambda x: list(x)[:limit],
-         'prediction': lambda x: list(x)[:limit]
-         }
+    preds_df = (
+        df.reset_index(1)
+        .groupby("customer_id")
+        .agg({"item_id": lambda x: list(x)[:limit], "prediction": lambda x: list(x)[:limit]})
     )
 
     return preds_df
 
 
-def load_model(model_path='model.pkl'):
-    with open(model_path, 'rb') as f:
+def load_model(model_path="model.pkl"):
+    with open(model_path, "rb") as f:
         model = pickle.load(f)
     return model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--file', type=str, required=True)
-    argparser.add_argument('--out', type=str, default='predictions.csv')
-    argparser.add_argument('--model_path', type=str, default='model.pkl')
+    argparser.add_argument("--file", type=str, required=True)
+    argparser.add_argument("--out", type=str, default="predictions.csv")
+    argparser.add_argument("--model_path", type=str, default="model.pkl")
     args = argparser.parse_args()
 
     model = load_model()
